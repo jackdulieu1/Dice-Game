317606507be79720d81b8ad16d25e7af7bfb9a8f
jackdulieu1 <61517434+jackdulieu1@users.noreply.github.com>
2020-11-17 14:30:49 +0000

    Added initial files

---
+++ index.js
@@ -0,0 +1,34 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var diceImage1 = "dice" + randomNumber1 + ".png";
+
+var imageSource1 = "images/" + diceImage1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", imageSource1);
+
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var diceImage2 = "dice" + randomNumber2 + ".png";
+
+var imageSource2 = "images/" + diceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", imageSource2);
+
+
+if (randomNumber1 >  randomNumber2){
+document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+}
+else if (randomNumber2 >  randomNumber1){
+  document.querySelector("h1").innerHTML = "🚩 Player 2 wins!";
+
+}
+
+else {
+  document.querySelector("h1").innerHTML = "🚩 It's a draw!";
+
+}
